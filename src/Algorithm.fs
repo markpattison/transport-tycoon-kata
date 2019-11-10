6f80b4acc2ba1e4752be3e423794478604518d92
markpattison <mr.pattison@gmail.com>
2019-11-10 12:36:03 +0000

    parsing destinations

--- src/Algorithm.fs
+++ src/Algorithm.fs
@@ -1,3 +1,23 @@
-module TransportTycoon.Algorithm
+namespace TransportTycoon
 
-let calculateHours x = 0
+type Warehouse = A | B
+
+type Place =
+    | Factory
+    | Port
+    | Warehouse of Warehouse
+
+module Algorithm =
+
+    let parseDestinations (input: string) =
+        input
+        |> Seq.map (fun c ->
+            match c with
+            | 'A' -> A
+            | 'B' -> B
+            | _ -> failwithf "Unknown destination: %c" c)
+        |> Seq.toList        
+
+    let calculateHours (input: string) =
+        let destinations = parseDestinations input
+        0
--- tests/Tests.fs
+++ tests/Tests.fs
@@ -1,13 +1,27 @@
 module TransportTycoon.Tests
 
+open System
 open NUnit.Framework
+
+[<Test>]
+let ``destinations parsed correctly`` () =
+    let input = "ABBA"
+    let expected = [ A; B; B; A ]
+    let result = Algorithm.parseDestinations input
+
+    Assert.AreEqual(expected, result)
+
+[<Test>]
+let ``invalid destinations throw an error`` () =
+    let input = "X"
+
+    Assert.Throws<Exception>(fun () -> Algorithm.parseDestinations input |> ignore)
+    |> ignore
 
 [<TestCase("A", 5)>]
 [<TestCase("AB", 5)>]
 [<TestCase("BB", 5)>]
 [<TestCase("ABB", 7)>]
-let Test1 input expected =
-
+let ``Sample cargo lists give correct answers`` input expected =
     let result = Algorithm.calculateHours input
-
     Assert.AreEqual(expected, result)
