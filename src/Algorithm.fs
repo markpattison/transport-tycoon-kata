da81428b383adfe4201aa464c00b68365ab5a7af
markpattison <mr.pattison@gmail.com>
2019-11-16 20:45:01 +0000

    refactor with some active patterns

--- src/Algorithm.fs
+++ src/Algorithm.fs
@@ -1,6 +1,6 @@
 module TransportTycoon.Algorithm
 
-let logging = false
+let logging = true
 
 let log state msg = if logging then printfn "Time %i: %s" state.Time msg
 
@@ -36,10 +36,6 @@ let unloadVehicle v =
     | Some cargo -> { v with Cargo = None }, cargo
     | None -> failwith "Cannot unload empty vehicle"
 
-let isArrivingAt time v =
-    match v.Location with
-    | Journey (_, _, loc, t) when t = time -> true
-    | _ -> false
 
 let splitFirstMatch predicate lst =
     let rec split acc l =
@@ -49,64 +45,88 @@ let splitFirstMatch predicate lst =
         | [] -> None
     split [] lst
 
-let emptyAt vehicleType location vehicle =
-    vehicle.Type = vehicleType && vehicle.Location = At location && vehicle.Cargo.IsNone
+let (|EmptyVehicleAt|_|) vehicleType location state =
+    match splitFirstMatch (fun v -> v.Type = vehicleType && v.Location = At location && v.Cargo.IsNone) state.Vehicles with
+    | Some (emptyVehicle, otherVehicles) -> Some (emptyVehicle, otherVehicles)
+    | _ -> None
 
-let loadedAt vehicleType location vehicle =
-    vehicle.Type = vehicleType && vehicle.Location = At location && vehicle.Cargo.IsSome
+let (|LoadedVehicleAt|_|) vehicleType location state =
+    match splitFirstMatch (fun v -> v.Type = vehicleType && v.Location = At location && v.Cargo.IsSome) state.Vehicles with
+    | Some (loadedVehicle, otherVehicles) -> Some (loadedVehicle, otherVehicles)
+    | _ -> None
+
+let (|CargoAt|_|) location state =
+    match state.Queues.[location] with
+    | cargoToLoad :: remainingCargo -> Some (cargoToLoad, remainingCargo)
+    | [] -> None
 
 let loadCargo vehicleType location state =
-    match splitFirstMatch (emptyAt vehicleType location) state.Vehicles, state.Queues.[location] with
-    | Some (emptyVehicle, otherVehicles), cargoToLoad :: remainingCargo ->
+    match state with
+    | EmptyVehicleAt vehicleType location (emptyVehicle, otherVehicles) & CargoAt location (cargoToLoad, remainingCargo) ->
         let loadedVehicle = { emptyVehicle with Cargo = Some cargoToLoad }
         sprintf "Loading: %O onto %O" cargoToLoad emptyVehicle |> log state
-        { state with Queues = state.Queues.Add(location, remainingCargo); Vehicles = loadedVehicle :: otherVehicles }
-    | _ -> state            
+        Some { state with Queues = state.Queues.Add(location, remainingCargo); Vehicles = loadedVehicle :: otherVehicles }
+    | _ -> None    
 
 let despatch vehicleType location findDestination state =
-    match splitFirstMatch (loadedAt vehicleType location) state.Vehicles with
-    | Some (loadedVehicle, otherVehicles) ->
+    match state with
+    | LoadedVehicleAt vehicleType location (loadedVehicle, otherVehicles) ->
         let destination = findDestination loadedVehicle.Cargo.Value
         let journey = (location, state.Time, destination, state.Time + distance location destination)
         let movingVehicle = { loadedVehicle with Location = Journey journey }
         sprintf "Despatching: %O, %O" loadedVehicle movingVehicle.Location |> log state
-        { state with Vehicles = movingVehicle :: otherVehicles }
-    | _ -> state
+        Some { state with Vehicles = movingVehicle :: otherVehicles }
+    | _ -> None    
 
 let unload vehicleType location state =
-    match splitFirstMatch (loadedAt vehicleType location) state.Vehicles with
-    | Some (loadedVehicle, otherVehicles) ->
+    match state with
+    | LoadedVehicleAt vehicleType location (loadedVehicle, otherVehicles) ->
         let unloadedVehicle, cargo = unloadVehicle loadedVehicle
         sprintf "Unloading: %O" loadedVehicle |> log state
-        { state with Queues = state.Queues.Add(location, cargo :: state.Queues.[location]); Vehicles = unloadedVehicle :: otherVehicles }
-    | _ -> state        
+        Some { state with Queues = state.Queues.Add(location, cargo :: state.Queues.[location]); Vehicles = unloadedVehicle :: otherVehicles }
+    | _ -> None    
 
 let returnEmpty vehicleType location destination state =
-    match splitFirstMatch (emptyAt vehicleType location) state.Vehicles with
-    | Some (emptyVehicle, otherVehicles) ->
+    match state with
+    | EmptyVehicleAt vehicleType location (emptyVehicle, otherVehicles) ->
         let journey = (location, state.Time, destination, state.Time + distance location destination)
         let movingVehicle = { emptyVehicle with Location = Journey journey }
         sprintf "Returning: %O, %O" emptyVehicle movingVehicle.Location |> log state
-        { state with Vehicles = movingVehicle :: otherVehicles }
-    | _ -> state
+        Some { state with Vehicles = movingVehicle :: otherVehicles }
+    | _ -> None    
 
-let arrive state =
-    match splitFirstMatch (isArrivingAt state.Time) state.Vehicles with
-    | Some (arrivingVehicle, otherVehicles) ->
-        let location = match arrivingVehicle.Location with | Journey (_, _, loc, _) -> loc | _ -> failwith "Location not found"
-        let arrived = { arrivingVehicle with Location = At location }
-        sprintf "Arriving: %O" arrivingVehicle |> log state
-        { state with Vehicles = arrived :: otherVehicles }
-    | _ -> state        
+let arriveAll state =
+    let updatedVehicles =
+        state.Vehicles
+        |> List.map (fun v ->
+            match v.Location with
+            | Journey (_, _, location, t) when t = state.Time ->
+                sprintf "Arriving: %O" v |> log state
+                { v with Location = At location }
+            | _ -> v)
+    { state with Vehicles = updatedVehicles }
 
 let timePasses state =
     let tNext = state.Time + 1
     if tNext > 50 then failwithf "*** TIME 50 ***/n%O" state
     { state with Time = tNext }
 
-let possibleActions =
-    [ arrive
-      loadCargo Truck Factory
+let isCompleted state =
+    state.Queues.[Factory].IsEmpty && state.Queues.[Port].IsEmpty && List.forall (fun v -> v.Cargo.IsNone) state.Vehicles
+
+let run rules originalState =
+    let rec runRec state =
+        if isCompleted state then
+            state
+        else
+            let state' = arriveAll state
+            match List.tryPick (fun f -> f state') rules with
+            | Some state'' -> runRec state''
+            | None -> runRec (timePasses state')
+    runRec originalState
+
+let scenarioRules =
+    [ loadCargo Truck Factory
       loadCargo Ship Port
       despatch Truck Factory (fun cargo -> match cargo with | Destination A -> Port | Destination B -> Warehouse B)
       despatch Ship Port (fun _ -> Warehouse A)
@@ -116,27 +136,7 @@ let possibleActions =
       returnEmpty Truck Port Factory
       returnEmpty Ship (Warehouse A) Port
       returnEmpty Truck (Warehouse B) Factory
-      timePasses // must be last
     ]
-
-let updatedStateIfDifferent state action =
-    let updated = action state
-    if updated = state then None else Some updated
-
-let tryUpdate state =
-    let updatedOpt = List.tryPick (updatedStateIfDifferent state) possibleActions
-    updatedOpt
-
-let isCompleted state =
-    state.Queues.[Factory].IsEmpty && state.Queues.[Port].IsEmpty && List.forall (fun v -> v.Cargo.IsNone) state.Vehicles
-
-let rec run state =
-    if isCompleted state then
-        state
-    else
-        match tryUpdate state with
-        | Some updated -> run updated
-        | None -> failwith "Uh oh"
 
 let calculateHours (input: string) =
     let destinations = parseDestinations input
@@ -144,6 +144,6 @@ let calculateHours (input: string) =
 
     let state = initialState cargo
     
-    let completed = run state
+    let completed = run scenarioRules state
 
     completed.Time
