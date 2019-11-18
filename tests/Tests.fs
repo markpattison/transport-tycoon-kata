e33c5cfa06a036c1a9a0e9c8e9360b9552537f9c
markpattison <mr.pattison@gmail.com>
2019-11-18 13:46:25 +0000

    regression tests for Exercise 1 logs

--- tests/Tests.fs
+++ tests/Tests.fs
@@ -57,3 +57,24 @@ let ``split first match works correctly when no matches present`` () =
 let ``Exercise 1 sample cargo lists give correct answers`` input expected =
     let result = input |> Program.parseDestinations |> Exercise1.calculateHours dummyLog
     Assert.AreEqual(expected, result)
+
+[<Test>]
+let ``logged events are as expected`` ()=
+    let input = "AB"
+    let logger = Program.Logger.Create
+
+    let expected = """{"event": "DEPART", "time": 0, "transport_id": 0, "kind": "TRUCK", "location": "FACTORY", "destination": PORT, "cargo": [{"cargo_id": 0, "destination": A, "origin": "FACTORY"}]}
+{"event": "DEPART", "time": 0, "transport_id": 1, "kind": "TRUCK", "location": "FACTORY", "destination": B, "cargo": [{"cargo_id": 1, "destination": B, "origin": "FACTORY"}]}
+{"event": "ARRIVE", "time": 1, "transport_id": 0, "kind": "TRUCK", "location": "PORT", "cargo": [{"cargo_id": 0, "destination": A, "origin": "FACTORY"}]}
+{"event": "DEPART", "time": 1, "transport_id": 2, "kind": "SHIP", "location": "PORT", "destination": A, "cargo": [{"cargo_id": 0, "destination": A, "origin": "FACTORY"}]}
+{"event": "DEPART", "time": 1, "transport_id": 0, "kind": "TRUCK", "location": "PORT", "destination": FACTORY}
+{"event": "ARRIVE", "time": 2, "transport_id": 0, "kind": "TRUCK", "location": "FACTORY"}
+{"event": "ARRIVE", "time": 5, "transport_id": 2, "kind": "SHIP", "location": "A", "cargo": [{"cargo_id": 0, "destination": A, "origin": "FACTORY"}]}
+{"event": "ARRIVE", "time": 5, "transport_id": 1, "kind": "TRUCK", "location": "B", "cargo": [{"cargo_id": 1, "destination": B, "origin": "FACTORY"}]}
+{"event": "DEPART", "time": 5, "transport_id": 2, "kind": "SHIP", "location": "A", "destination": PORT}
+{"event": "DEPART", "time": 5, "transport_id": 1, "kind": "TRUCK", "location": "B", "destination": FACTORY}"""
+
+    input |> Program.parseDestinations |> Exercise1.calculateHours logger.Add |> ignore
+    let logResult = String.Join("\n", logger.Logs)
+
+    Assert.AreEqual(expected, logResult)
